458d5eb665f3d07416546cff234d6342476bca61
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-06-13 09:46:44 +0300

    Commit

---
+++ project006/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`listening on *:${port}`);
+});
